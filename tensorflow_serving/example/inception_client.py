db4bad82a572b1ff0e014e01fafa098853707bb5
David Lea <lea@neon-lab.com>
2016-04-20 16:10:59 -0700

    added PIL-based preprocessing functions as well as a testing function (will be removed)

--- tensorflow_serving/example/inception_client.py
+++ tensorflow_serving/example/inception_client.py
@@ -27,6 +27,8 @@ import threading
 from grpc.beta import implementations
 import numpy
 import tensorflow as tf
+from tensorflow.python.platform.logging import warn
+from PIL import Image
 
 from tensorflow_serving.example import inception_inference_pb2
 
@@ -34,13 +36,159 @@ from tensorflow_serving.example import inception_inference_pb2
 tf.app.flags.DEFINE_string('server', 'localhost:9000',
                            'inception_inference service host:port')
 tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
+tf.app.flags.DEFINE_integer('image_size', 299,
+              """Needs to provide same value as in training.""")
+tf.app.flags.DEFINE_string('prep_method', 'resize',
+               '''Defines the method used to get images to image_size:
+                - resize: Resize the image (distortion, whole image, no blank 
+                  space)
+                - crop: Center-crop the image to image_size (no distortion, 
+                  partial image, no blank space)
+                - padresize: Pads the image to the appropriate aspect ratio 
+                then resizes (no distortion, whole image, blank space)''')
+
 FLAGS = tf.app.flags.FLAGS
 
+if FLAGS.prep_method not in ['resize', 'crop', 'padresize']:
+  warn('Preprocessing method "%s" is unknown. Defaulting to resize.', 
+       FLAGS.prep_method)
+  FLAGS.prep_method = 'resize'
 
 NUM_CLASSES = 5
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 SYNSET_FILE = os.path.join(WORKING_DIR, 'imagenet_lsvrc_2015_synsets.txt')
 METADATA_FILE = os.path.join(WORKING_DIR, 'imagenet_metadata.txt')
+
+
+def prep_image(img, w=FLAGS.image_size, h=FLAGS.image_size):
+  '''
+  Preprocesses the requested image to the desired size, permitting server-side
+  batching of Inception.
+  '''
+  if FLAGS.prep_method == 'resize':
+    return _resize_to(img, w=w, h=h)
+  elif FLAGS.prep_method == 'crop':
+    # resize to appropriate dimensions
+    resized_im = _resize_to_min(img, w=w, h=h)
+    # center crop
+    return _center_crop_to(resized_im, w=w, h=h)
+  elif FLAGS.prep_method == 'padresize':
+    des_asp = float(w) / h
+    # pad the image
+    padded_im = _pad_to_asp(img, des_asp)
+    # resize the image
+    return _resize_to(padded_im, w=w, h=h)
+
+
+def _resize_to(img, w=None, h=None):
+  '''
+  Resizes the image to a disired width and height. If either is undefined,
+  it resizes such that the defined argument is satisfied and preserves aspect
+  ratio. If both are defined, resizes to satisfy both arguments without
+  preserving aspect ratio.  
+
+  Args:
+    img: A PIL image.
+    w: The desired width.
+    h: The desired height.
+  '''
+  ow, oh = img.size
+  asp = float(ow) / oh
+  if w is None and h is None:
+    # do nothing
+    return img
+  elif w is None:
+    # set the width
+    w = int(h * asp)
+  elif h is None:
+    h = int(w / asp)
+  return img.resize((w, h), Image.BILINEAR)
+
+
+def _resize_to_min(img, w=None, h=None):
+  '''
+  Resizes an image so that its size in both dimensions is greater than or 
+  equal to the provided arguments. If either argument is None, that dimension 
+  is ignored. If the image is larger in both dimensions, then the image is
+  shrunk. In either case, the aspect ratio is preserved and image size is
+  minimized.
+
+  Args:
+    img: A PIL image.
+    w: The minimum width desired.
+    h: The minimum height desired.
+  '''
+  ow, oh = img.size
+  if w is None and h is None:
+    return img
+  if w is None:
+    # resize to the desired height
+    return _resize_to(img, h=h)
+  elif h is None:
+    # resize to the desired width
+    return _resize_to(img, w=w)
+  if ow == w and oh == h:
+    # then you need not do anything
+    return img
+  hf = h / float(oh)  # height scale factor
+  wf = w / float(ow)  # width scale factor
+  if min(hf, wf) < 1.0:
+    # then some scaling up is necessary. Scale up by as much as needed,
+    # leaving one dimension larger than the requested amount if required.
+    scale_factor = max(hf, wf)
+  else:
+    # scale down by the least amount to ensure both dimensions are larger
+    scale_factor = min(hf, wf)
+  nw = int(ow * scale_factor)
+  nh = int(oh * scale_factor)
+  return _resize_to(img, w=nw, h=nh)
+
+
+def _center_crop_to(img, w, h):
+  '''
+  Center crops image to desired size. If either dimension of the image is
+  already smaller than the desired dimensions, the image is not cropped.
+
+  Args:
+    img: A PIL image.
+    w: The width desired.
+    h: The height desired.
+  '''
+  ow, oh = img.size
+  if ow < w or oh < h:
+    return img
+  upper = (h - oh) / 2
+  lower = cy1 + h
+  left = (w - ow) / 2
+  right = cx1 + w
+  return img.crop((left, upper, right, lower))
+
+
+def _pad_to_asp(img, asp):
+  '''
+  Symmetrically pads an image to have the desired aspect ratio.
+
+  Args:
+    img: A PIL image.
+    asp: The aspect ratio, a float, as w / h
+  '''
+  ow, oh = img.size
+  oasp = float(ow) / oh
+  if asp > oasp:
+    # the image is too narrow. Pad out width.
+    nw = int(oh * asp)
+    left = (nw - ow) / 2
+    upper = 0
+    newsize = (nw, oh)
+  elif asp < oasp:
+    # the image is too short. Pad out height.
+    nh = int(ow / asp)
+    left = 0
+    upper = (nh - oh) / 2
+    newsize = (ow, nh)
+  nimg = Image.new(img.mode, newsize)
+  nimg.paste(img, box=(left, upper))
+  return nimg
 
 
 def main(_):
@@ -69,6 +217,25 @@ def main(_):
       index = result.classes[i]
       score = result.scores[i]
       print '%f : %s' % (score, texts[synsets[index - 1]])
+
+
+def checkerboard(w=256, h=256, c0=0, c1=255, blocksize=16):
+  '''
+  Generates a checkerboard pattern, for testing the preprocessing.
+
+  Returns the pattern as a PIL image.
+  '''
+  block = numpy.tile([1], (blocksize, blocksize))
+  block1 = block * c0
+  block2 = block * c1
+  sb1 = numpy.hstack([block1, block2])
+  sb2 = numpy.hstack([block2, block1])
+  sb = numpy.vstack([sb1, sb2])
+  d = blocksize * 2
+  rep_w = numpy.ceil(float(w) / d)
+  rep_h = numpy.ceil(float(h) / d)
+  board = numpy.tile(sb, (rep_h, rep_w))[:h, :w]
+  return Image.fromarray(board.astype(float))
 
 
 if __name__ == '__main__':
