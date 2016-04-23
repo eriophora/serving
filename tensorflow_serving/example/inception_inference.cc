ff218bb5ab15d7455e2f1407b0dba22563b9d218
David Lea <lea@neon-lab.com>
2016-04-22 17:23:08 -0700

    changed the serving parameters and timeout in hopes of getting rid of the this-task-would-start-a-fresh-batch thing

--- tensorflow_serving/example/inception_inference.cc
+++ tensorflow_serving/example/inception_inference.cc
@@ -213,7 +213,8 @@ InceptionServiceImpl::InceptionServiceImpl(
   // specific graph structure and usage.
   tensorflow::serving::StreamingBatchScheduler<Task>::Options scheduler_options;
   scheduler_options.thread_pool_name = "inception_service_batch_threads";
-  // scheduler_options.batch_timeout_micros = 5000 * 1000;  // five seconds
+  scheduler_options.batch_timeout_micros = 500 * 1000;  // half a second
+  scheduler_options.num_batch_threads = 10;
   tensorflow::serving::BatchSchedulerRetrier<Task>::Options retry_options;
   // Retain the default retry options.
   TF_CHECK_OK(tensorflow::serving::CreateRetryingStreamingBatchScheduler<Task>(
